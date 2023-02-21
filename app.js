a0dd3c8255ca0eb2477f4f3b4632c81ebb1ec0b4
isaacdong88 <isaacdong88@gmail.com>
2023-02-21 11:23:52 -0500

    linked all files, created wireframe, designed basic html gameboard

---
+++ app.js
@@ -0,0 +1,15 @@
+// Create A gameboard array with three separate arrays for 3rows
+
+let gameBoard = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]
+
+console.log(gameBoard)
+
+// Create Player1 and Player2 variables acts as on/off switch between turns
+
+// Create function that will track which spot each player go and determines either to draw a 'X' or an 'O' on the board. The same function will also check each time the 8 winning combinations, need 8 if statements. If there is a combination thats have all three matches declare the winner and end the match.
+
+//Create a button for play again that resets the game
